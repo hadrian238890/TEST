7e418f218963cc14595f2d174005cee7efe3b1c3
hadrian <hadrianpoland@gmail.com>
2019-01-05 19:18:37 +0100

    05.01

--- test.cpp
+++ test.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
 
+void foo(string word)
+{	
+	for(int i = word.size(); i > 0; i--)
+	{
+		cout << word[i];
+	}
+}
+
+
 int main()
 {
+	string letter;
 	cout << "Hello world!" << endl;
+	cout << "Type a word: ";
+	cin >> letter;
+
+	foo(letter);
+
 	return 0;
 }
