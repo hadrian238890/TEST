e21fffca2f90742cab58618d6185c4a97c66c868
hadrian <hadrianpoland@gmail.com>
2019-01-06 18:27:07 +0100

    05.0105.0105.0105.0105.01

--- test.cpp
+++ test.cpp
@@ -1,18 +1,65 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <set>
 
 using namespace std;
 
-void foo(string word)
+void backToFront(string word)
 {	
-	for(int i = word.size(); i > 0; i--)
+	for(int i = word.size(); i > -1; i--)
 	{
 		cout << word[i];
 	}
+	cout << endl;
 }
 
+bool checkIfSameLetter1(string word)
+{
+	cout << "Set container method " << endl;
+	cout << "Are there any duplicated letters? " << endl;
+	set<char> str;
+	for(int i = 0; i < word.size(); i++)
+	{
+		 str.insert(word[i]);
+	}
+	if(str.size() == word.size())
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+	 
+}
 
+bool checkIfSameLetter2(string word)
+{
+	cout << "2 Loops method" << endl;
+	cout << "Are there any duplicated letters? " << endl;
+	int counter = 0;
+	for(int i = 0; i <= word.size(); i++)
+	{
+		for(int j = i+1; j <= word.size(); j++)
+		{
+			if(word[i] == word[j])
+			{
+				counter++;
+			}
+		}
+	}
+
+	if(counter > 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+	cout << endl;
+}
 int main()
 {
 	string letter;
@@ -20,7 +67,8 @@ int main()
 	cout << "Type a word: ";
 	cin >> letter;
 
-	foo(letter);
-
+	backToFront(letter);
+	cout << checkIfSameLetter1(letter) << endl;
+	cout << checkIfSameLetter2(letter) << endl; 
 	return 0;
 }
